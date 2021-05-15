769ca9c24cb79e27fe6fb5622e88bcec3f25667e
Abhinav Kumar Verma <oyeluckydpsvaranasi@gmail.com>
2021-05-16 02:09:55 +0530

    Remove bugs of CAnetwork. Add support functions.

--- CAnetwork.py
+++ CAnetwork.py
@@ -1,6 +1,8 @@
 import random
 from CAnode import CAnode
 from support import *
+import warnings
+import pandas as pd
 
 def construct_fully_connected_graph(num_nodes):
     graph = {}
@@ -54,11 +56,22 @@ def construct_tesseract(dimension, naming_convention = 'number'):
 
 class CAnetwork:
 
-    def __init__(self, graph = None, node_values = None, incoming_operator=None, post_reg_operator=None, msg = ''):
+    def __init__(self, graph = None, node_values = 'random', incoming_operator=None, post_reg_operator=None, msg = ''):
+        '''
+        :param graph: A dictionary graph in form of node_name:list_of_outgoing_edge_node.
+        :param node_values: Either a list of init_values for nodes in the same order as node_name in the dictionary or
+                            an initialization string like 'random'/...
+        :param incoming_operator: Pointer to the incoming oprerator function.
+        :param post_reg_operator: POint to the post register operator function.
+        :param msg: Message to be a part of object display.
+        '''
         if graph is not None:
             self.network = graph
             self.nodes = {}
             self.msg = ''
+            if type(node_values) is not list:
+                if node_values == 'random':
+                    node_values = [random.randint(0, 1) for i in range(len(graph))]
             for (node_name, edges), value in zip(self.network.items(), node_values):
                 self.nodes[node_name] = CAnode(node_name, value, incoming_operator=incoming_operator, post_reg_operator=post_reg_operator)
             for node_name, edges in self.network.items():
@@ -69,26 +82,39 @@ class CAnetwork:
             self.network = {}
             self.nodes = {}
 
-    def add_node(self, name, val, outgoing_nodes = None, incoming_nodes = None, *args, **kwargs):
+    def convert_to_nodes(self, node_list):
+        '''
+        :param node_list: A list of combination of names and nodes
+        :return: The list is converted to a list of nodes only.
+        '''
+        return [self.nodes[node] if type(node) is not CAnode else node for node in node_list]
+
+    def add_node(self, name, val = 'random', outgoing_nodes = None, incoming_nodes = None, *args, **kwargs):
         '''
         :param name: Name of node to add
-        :param val: Value of node to add
+        :param val: Value of node to add or a seed to value.
         :param outgoing_nodes: list of outgoing nodes
         :param incoming_nodes: lsit of incoming nodes
         :return:
         '''
         # Raise error if name already exists.
         if name in self.nodes.keys():
-            raise KeyError
+            raise KeyError('Node name already exists!')
+        if val not in [0, 1]:
+            if val == 'random':
+                val = random.randint(0, 1)
 
         # Create new node and add it to the network.
-        new_node = CAnode(name, val, outgoing_nodes=outgoing_nodes, incoming_nodes=incoming_nodes, *args, **kwargs)
+        outgoing_nodes = self.convert_to_nodes(outgoing_nodes)
+        incoming_nodes = self.convert_to_nodes(incoming_nodes)
+        new_node = CAnode(name, val, outgoing_nodes=None, incoming_nodes=None, *args, **kwargs)
         self.nodes[name] = new_node
-        if outgoing_nodes:
-            self.network[name] = [node.name for node in outgoing_nodes]
-        if incoming_nodes:
-            for node in incoming_nodes:
-                self.network[node.name].append(name)
+        self.network[name] = []
+
+        # Use add edge feature here.
+        from_list = [i.name for i in incoming_nodes] + [name]*len(outgoing_nodes)
+        to_list = [name]*len(incoming_nodes) + [i.name for i in outgoing_nodes]
+        self.add_edge(from_list, to_list)
 
     def is_edge(self, node, to_node):
         return to_node.name in self.network[node.name]
@@ -108,10 +134,13 @@ class CAnetwork:
         if dir == 'bi':
             from_nodes = node + node_edge
             to_nodes = node_edge + node
+        else:
+            from_nodes = node
+            to_nodes = node_edge
 
         # Convert to ndoes if node names are passed in list.
-        from_nodes = [self.nodes[node] if type(node) is not CAnode else node for node in from_nodes]
-        to_nodes = [self.nodes[node] if type(node) is not CAnode else node for node in to_nodes]
+        from_nodes = self.convert_to_nodes(from_nodes)
+        to_nodes = self.convert_to_nodes(to_nodes)
 
         # Iterate over lists to add edges.
         for from_node, to_node in zip(from_nodes, to_nodes):
@@ -132,9 +161,11 @@ class CAnetwork:
         name = node.name
 
         # Prepare list of edges to remove
-        to_list = self.network[name]
+        to_list = [i for i in self.network[name]]
         from_list = [name]*len(to_list)
         for from_node_name, to_node_name_list in self.network.items():
+            if from_node_name == name:
+                continue
             if name in to_node_name_list:
                 from_list.append(from_node_name)
                 to_list.append(name)
@@ -142,10 +173,10 @@ class CAnetwork:
         # Remove the edges
         if to_list and from_list:
             self.remove_edge(from_list, to_list)
-
-        # Remove node from self dictionaries.
-        del self.network[name]
-        del self.nodes[name]
+        else:
+            # If there is no edge to remove, remove node from self dictionaries.
+            del self.network[name]
+            del self.nodes[name]
 
 
     def remove_edge(self, node, node_edge):
@@ -157,32 +188,45 @@ class CAnetwork:
         # If single node is passed, make a list out of lt.
         if type(node) is not list:
             from_nodes = [node]
+        else:
+            from_nodes = node
         if type(node_edge) is not list:
             to_nodes = [node_edge]
+        else:
+            to_nodes = node_edge
 
         # Convert to ndoes if node names are passed in list.
-        from_nodes = [self.nodes[node] if type(node) is not CAnode else node for node in from_nodes]
-        to_nodes = [self.nodes[node] if type(node) is not CAnode else node for node in to_nodes]
+        from_nodes = self.convert_to_nodes(from_nodes)
+        to_nodes = self.convert_to_nodes(to_nodes)
 
         # Iterate over the list and remove edge one by one.
         for from_node, to_node in zip(from_nodes, to_nodes):
             if not self.is_edge(from_node, to_node):
-                raise ValueError
-            from_node.remove_outgoing_nodes(to_node)
-            to_node.remove_incoming_nodes(from_node)
+                raise ValueError('Edge does not exist!')
+            from_node.remove_outgoing_node(to_node)
+            to_node.remove_incoming_node(from_node)
             self.network[from_node.name].remove(to_node.name)
 
             # Check for isolated node and remove it.
+            # Introducing a known bug here. Even if non-disconnected node is called with remove_node function, it would result in FLoating node warning.
+            # The following code only removes isolated nodes, but it cannot take care of bifurcated graph.
             if len(self.network[from_node.name])==0 and \
                 all([from_node.name not in to_nodes_list if node_name is not from_node.name else True for node_name, to_nodes_list in self.network.items()]):
+                msg = 'Removing a floating node! Node = ' + from_node.__repr__()
+                warnings.warn(msg)
                 self.remove_node(from_node)
+            if len(self.network[to_node.name])==0 and \
+                all([to_node.name not in to_nodes_list if node_name is not to_node.name else True for node_name, to_nodes_list in self.network.items()]):
+                msg = 'Removing a floating node! Node = ' + to_node.__repr__()
+                warnings.warn(msg)
+                self.remove_node(to_node)
 
     def set_incoming_operator(self, incoming_operator):
-        for _, node in self.nodes.items():
+        for _, node in self:
             node.set_incoming_operator(incoming_operator=incoming_operator)
 
     def set_post_reg_operator(self, post_reg_operator):
-        for _, node in self.nodes.items():
+        for _, node in self:
             node.set_post_reg_operator(post_reg_operator=post_reg_operator)
 
     def process_network(self):
@@ -190,22 +234,38 @@ class CAnetwork:
         :return: processing each node of network
         operator on the output of first process and stored value.
         '''
-        for _, node in self.nodes.items():
+        for _, node in self:
             node.process_node()
 
     def enact_value(self):
-        for _, node in self.nodes.items():
+        for _, node in self:
             node.enact_value()
+        return self.network_value
 
     def step_the_network(self):
         self.process_network()
         self.enact_value()
+        self.log_value()
+        return self.network_value
+
+    @property
+    def network_value(self):
+        network_value = []
+        for _, node in self:
+            network_value.append(node.value)
+        return network_value
 
     def __iter__(self):
         return iter(self.nodes.items())
 
     def __getitem__(self, item):
         return self.nodes[item]
+
+    def __setitem__(self, key, value):
+        self.nodes[key] = value
+
+    def __len__(self):
+        return len(self.nodes)
 
     def __str__(self, prefix = '', indent = '\t'):
         total_indent = prefix
@@ -217,7 +277,7 @@ class CAnetwork:
 
     def __repr__(self):
         edges = sum([len(v) for _,v in self.network.items()])
-        message = '<'+str(id(self))+': '+str(len(self.nodes))+' nodes, '+str(edges)+' edges'
+        message = '<'+str(id(self))+': '+str(len(self))+' nodes, '+str(edges)+' edges'
         if self.msg:
             message += ', msg = ' + self.msg + '>'
         else:
@@ -231,5 +291,14 @@ if __name__ == '__main__':
     graph_6 = construct_tesseract(6, 'binary')
     # node1 = CAnode(1,1)
     # print(node1.__repr__())
-    network = CAnetwork(graph, [random.randint(0, 1) for i in range(len(graph))])
+    network = CAnetwork(graph, 'random')
+    network.add_node('NN1', 0, incoming_nodes=[63], outgoing_nodes=[126])
+    network.add_edge('NN1', 0)
+    network.add_edge(0, 'NN1')
+    network.add_node('NN2', 'random', incoming_nodes=[63, 'NN1'], outgoing_nodes=[126, 'NN1'])
+    print(network)
+    network.remove_node('NN1')
+    network.remove_node(63)
+    network.remove_node(126)
+    print(network)
     pass
